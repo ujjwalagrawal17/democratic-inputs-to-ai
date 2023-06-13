1d16197a0294bee759bb329deeda70334c2014e9
Ujjwal Agrawal <ujjwal@codenicely.in>
2023-06-13 18:30:03 +0530

    added requirements file

---
+++ server/account/management/command/cluster_users.py
@@ -0,0 +1,56 @@
+from django.core.management import BaseCommand
+
+from account.models import User
+from polls.models import UserInput
+
+
+class Command(BaseCommand):
+    help = 'Command to cluster users based on their opinions'
+
+    def handle(self, *args, **options):
+        user_inputs = UserInput.objects.all()
+        user_input_json = [
+            {
+                "question": 1,
+                "user": 1,
+                "statement_shown_to_user": "some statement shown to the user",
+                "input": "agree",
+                "answer": "users answer that he described in detail",
+            },
+            {
+                "question": 1,
+                "user": 2,
+                "statement_shown_to_user": "some statement shown to the user",
+                "input": "agree",
+                "answer": "users answer that he described in detail",
+            },
+            {
+                "question": 1,
+                "user": 3,
+                "statement_shown_to_user": "some statement shown to the user",
+                "input": "agree",
+                "answer": "users answer that he described in detail",
+            },
+            {
+                "question": 2,
+                "user": 1,
+                "statement_shown_to_user": "some statement shown to the user",
+                "input": "agree",
+                "answer": "users answer that he described in detail",
+            },
+            {
+                "question": 2,
+                "user": 2,
+                "statement_shown_to_user": "some statement shown to the user",
+                "input": "agree",
+                "answer": "users answer that he described in detail",
+            },
+            {
+                "question": 2,
+                "user": 3,
+                "statement_shown_to_user": "some statement shown to the user",
+                "input": "agree",
+                "answer": "users answer that he described in detail",
+            },
+        ]
+
